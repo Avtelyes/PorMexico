740548904d06928b642d1e63d13190ed9f22dc97
Arturo Burela <uno@arturoburela.com>
2017-09-21 17:05:34 -0500

    Nested location

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,5 @@ class Location < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode, :if => :address_changed? or :longitude_changed?
   belongs_to :post
+  accepts_nested_attributes_for :post
 end
